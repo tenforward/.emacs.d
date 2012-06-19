2be0923d9c2ee755a8095762287b5df3f599a82c
KATOH Yasufumi <karma@jazz.email.ne.jp>
2012-06-19 13:38:56 +0900

    add config for mac

---
+++ site-start.d/mac.el
@@ -0,0 +1,13 @@
+;; assign command key to meta.
+(setq ns-command-modifier (quote meta))
+;; assign option key to command.
+;(setq ns-alternate-modifier (quote super))
+
+;; Drag & Drop
+;(define-key global-map [ns-drag-file] 'ns-find-file)
+
+;; Transparent frame
+;(set-frame-parameter (selected-frame) 'alpha '(90 50))
+
+;; Enable Ctrl+Space
+;(mac-add-ignore-shortcut ¡Æ(ctl ? ))
