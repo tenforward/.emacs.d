2d91415d93b870375cc4a0e1eaf5ba91b99b7f8d
KATOH Yasufumi <karma@jazz.email.ne.jp>
2013-10-11 15:35:57 +0900

    Update

--- site-start.d/mozc.el
+++ site-start.d/mozc.el
@@ -6,3 +6,8 @@
 (global-set-key (kbd "M-SPC") 'toggle-input-method)
 ; Alt-Space
 ;(global-set-key (kbd "A-SPC") 'toggle-input-method)
+
+(set-face-attribute 'mozc-cand-overlay-even-face 'nil
+                    :background "LightGray" :foreground "navy")
+(set-face-attribute 'mozc-cand-overlay-odd-face 'nil
+                    :background "LightGray" :foreground "navy")
