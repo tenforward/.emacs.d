89a2901f351168983d38e269b5da9778ce88a8be
KATOH Yasufumi <karma@jazz.email.ne.jp>
2013-11-16 03:04:33 +0900

    MacOS X Mavericks の場合のみ，カレントディレクトリを設定するようにした

--- init.el
+++ init.el
@@ -1,5 +1,14 @@
 (add-to-list 'load-path "~/.emacs.d/lisp.d/")
 
+(when (eq system-type 'darwin)
+  (setq inhibit-splash-screen t)
+  (defun cd-to-homedir-all-buffers ()
+    "Change every current directory of all buffers to the home directory."
+    (mapc
+     (lambda (buf) (set-buffer buf) (cd (expand-file-name "~"))) (buffer-list)))
+  (add-hook 'after-init-hook 'cd-to-homedir-all-buffers)
+  )
+
 ;; ~/.emacs.d/init.d 以下にある *.el ファイルを読み込む
 (let* ((dir "~/.emacs.d/site-start.d")
        (el-suffix "\\.el\\'")
