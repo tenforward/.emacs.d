d4d712dd6feeefd73fae4c5d7c21dc61a141a7e8
KATOH Yasufumi <karma@jazz.email.ne.jp>
2014-05-01 16:04:19 +0900

    mozc.el : Super-Space で Mozc が起動するようにした

--- site-start.d/mozc.el
+++ site-start.d/mozc.el
@@ -3,9 +3,11 @@
 (setq mozc-candidate-style 'overlay)
 
 ; meta-Space
-(global-set-key (kbd "M-SPC") 'toggle-input-method)
+;(global-set-key (kbd "M-SPC") 'toggle-input-method)
 ; Alt-Space
 ;(global-set-key (kbd "A-SPC") 'toggle-input-method)
+; super-Space
+(global-set-key (kbd "s-SPC") 'toggle-input-method)
 
 (set-face-attribute 'mozc-cand-overlay-even-face 'nil
                     :background "LightGray" :foreground "navy")
