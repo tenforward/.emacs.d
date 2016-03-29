97c56e40354aecdfc7f358ebb94990fdcacf2c05
KATOH Yasufumi <karma@jazz.email.ne.jp>
2016-03-29 18:12:11 +0900

    major.el: remove configration related euc-jp
    LANGをUTF-8に変更したため

--- site-start.d/major.el
+++ site-start.d/major.el
@@ -13,13 +13,13 @@
 ;;	     (c-set-style "stroustrup")
 ;;	     (c-set-style "whitesmith")
 ;;	     (c-set-style "ellemtel")
-;;	     (c-set-style "linux")
+	     (c-set-style "linux")
 	     ; 漢字を追加した時に…，
 	     ; 文字コードを質問してから保存してほしい場合．
 ;;	     (my-set-file-encoding default-buffer-file-coding-system)
 	     ; EUCで速攻で保存する場合．
-	     (my-set-file-encoding-quick 'euc-japan)
-	     (setq c-basic-offset 4)
+	     ;(my-set-file-encoding-quick 'euc-japan)
+;;	     (setq c-basic-offset 4)
 ;;	     (setq tab-width 4)
 ;;	     (setq c-auto-newline t)
 	     ) t)
@@ -35,7 +35,7 @@
 	     ; 文字コードを質問してから保存してほしい場合．
 ;;	     (my-set-file-encoding default-buffer-file-coding-system)
 	     ; EUCで速攻で保存する場合．
-	     (my-set-file-encoding-quick 'euc-japan)
+	     ;(my-set-file-encoding-quick 'euc-japan)
 	     ) t)
 
 ;;; navi2ch
