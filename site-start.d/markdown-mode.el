f1afad89d674eb1b4c4271e0183bca98f52c6416
KATOH Yasufumi <karma@jazz.email.ne.jp>
2017-01-06 16:55:49 +0900

    modify markdown-mode config

--- site-start.d/markdown-mode.el
+++ site-start.d/markdown-mode.el
@@ -1,3 +1,7 @@
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+(add-hook 'markdown-mode-hook
+          '(lambda ()
+             (electric-indent-local-mode -1)))
