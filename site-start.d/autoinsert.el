8132b72d02d16f0b042f473e6cbe9a7165af4d3e
KATOH Yasufumi <karma@jazz.email.ne.jp>
2017-01-06 16:53:35 +0900

    modify auto-insert-mode setting
    * Insert template when TeX file is opened

--- site-start.d/autoinsert.el
+++ site-start.d/autoinsert.el
@@ -1,6 +1,21 @@
-(require 'autoinsert)
-(setq auto-insert-directory "~/template/")
+(auto-insert-mode)
+; テキストファイルからテンプレートを読み込む
+;(setq auto-insert-directory "~/template/")
+;(define-auto-insert "\\.tex$" "tex-template.tex")
+
+(setq auto-insert-alist nil)
 (setq auto-insert-alist
-      (nconc '(("\\.tex$" . "template.tex")
-                ) auto-insert-alist))
-(add-hook 'find-file-not-found-hooks 'auto-insert)
+      (append '(
+                (("\\.tex$" . "TeX template")
+                 nil ;; ここに"name?"とか書くと、最初にミニバッファで"name?"ってでて入力を促される。入力したものはstrという変数に入る。
+                 "%#! platex " (file-name-nondirectory (buffer-file-name)) " && dvipdfmx -d 5 " (substring (file-name-nondirectory (buffer-file-name)) 0 -4) "\n"
+                 "\n"
+		 "\\documentclass[]{}\n"
+		 "\\title{}\n"
+		 "\\author{}\n"
+		 "\\date{}\n"
+		 "\n"
+		 "\\begin{document}\n\n"
+                 _ ;; アンダーバーを書いたところにカーソルが移動する
+		 "\\end{document}\n"
+                 )) auto-insert-alist))
